30094ab470e8e14cb9b02bde17b5ac81a281e459
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-15 01:06:34 -0800

    Updated Calc.py

--- Calc.py
+++ Calc.py
@@ -1,8 +1,10 @@
+#python based calculator:
 #using functions:
 def calculator():
     op = input("enter a operation(+,-,*,/):")
     a = int(input("enter 1st number:"))
     b = int(input("enter 2nd number:"))
+    #checks operation by using conditional statements:
     if(op == "+"):
         print(a+b)
     elif(op == "-"):
@@ -16,4 +18,5 @@ def calculator():
             print("error:division by zero")
     else:
         print("invalid operation please try again !!!!")
+#function calling:
 calculator()
